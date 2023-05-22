9151bad81a0b0a4c73eb80c0761ed6e69f7f0011
Huy Nguyen <danghuy1999@gmail.com>
2023-05-22 21:21:35 +0700

    [c] Add static root

--- myapp/config/settings/base.py
+++ myapp/config/settings/base.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR.path("static")
 STATIC_URL = "static/"
 
 # Default primary key field type
